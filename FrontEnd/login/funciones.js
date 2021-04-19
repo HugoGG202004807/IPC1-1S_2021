d1e9f6c33e6e879d9507034781872c47749bf713
HugoGG202004807 <3056736960301@ingenieria.usac.edu.gt>
2021-04-18 20:47:25 -0600

    Pagina principal terminada, login casi terminado

---
+++ FrontEnd/login/funciones.js
@@ -0,0 +1,12 @@
+// funcion para registrar usuarios
+function CrearUsuario(){
+    var nombre = document.getElementById("nombre");
+    var apellido = document.getElementById("apellido");
+    var usuario = document.getElementById("usuario");
+    alert(nombre.value).getElementById("contraseña");
+    }
+
+function IniciarSesion(){
+    var user= document.getElementById("luser");
+    var pass= document.getElementById("lcontraseña")
+}
