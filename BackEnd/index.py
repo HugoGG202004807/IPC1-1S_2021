2add6f2d5a1716b8299fb7a19381103935c61f7a
HugoGG202004807 <3056736960301@ingenieria.usac.edu.gt>
2021-04-20 17:17:15 -0600

    comenzando el el BackEnd

---
+++ BackEnd/index.py
@@ -0,0 +1,21 @@
+#importaciones de Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+#crear la app
+app= Flask(__name__)
+app.config["DEBUG"]=True
+
+CORS(app)
+
+# EndPoints
+
+@app.route('/',methods=['GET'])
+def home():
+    return 'SERVER IS WORKING !!!'
+
+#INICIAR EL SERVIDOR 
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
