7c054f5335ee6d0ae9da8a99cb887173c5726c34
HugoGG202004807 <3056736960301@ingenieria.usac.edu.gt>
2021-05-03 16:51:07 -0600

    agregando un cambio de pruba para la nubre

--- BackEnd/Gestor.py
+++ BackEnd/Gestor.py
@@ -1,15 +1,22 @@
 from Pacientes import Paciente
 from Medicamentos import Medicamento
+from Doctores import Doctor
+from Enfermeras import Enfermera
 import json
+import re
 
 class Gestor:
     def __init__(self):
-        self.pasientes=[]
+        self.pacientes=[]
         self.medicamentos=[]
+        self.doctores=[]
+        self.enfermeras=[]
             
         self.medicamentos.append(Medicamento('ubuprofeno','5', 'para el dolor', '2 tabletas'))
-        self.pasientes.append(Paciente('hugo', 'giron','27/09/01','h','hdgg','admi1','7832' ))
-        self.pasientes.append(Paciente('katerine', 'martinez','27/09/01','m','kmgg','admi2','0503' ))
+        self.pacientes.append(Paciente('hugo', 'giron','27/09/01','h','hdgg','admi1','7832' ))
+        self.pacientes.append(Paciente('katerine', 'martinez','27/09/01','f','kmgg','admi2','0503' ))
+        self.doctores.append(Doctor('Alan', 'Zoto', '27/09/1985', 'm', 'jsim', '147', 'covid-19', '7536699'))
+        self.enfermeras.append(Enfermera('Andrea', 'hernandez', '27/09/2002', 'f', 'Anh69', '4567', '783205'))
 
 
 
@@ -17,18 +24,52 @@ class Gestor:
     def crearMedicamento(self,nombre, precio, descripcion, cantidad):
         self.medicamentos.append(Medicamento(nombre, precio, descripcion, cantidad))
 
+    def crearPaciente(self,nombre, apellido, nacimiento, genero, usuario, contraseña, telefono):
+        self.pacientes.append(Paciente(nombre, apellido, nacimiento, genero, usuario, contraseña, telefono))
+
+    def crearDoctor(self,nombre, apellido, nacimiento, genero, usuario, contraseña, especialidad, telefono):
+        self.doctores.append(Paciente(nombre, apellido, nacimiento, genero, usuario, contraseña, especialidad, telefono))    
+
+
     #Read
     def obtener_medicamento(self):
         return json.dumps([ob.__dict__ for ob in self.medicamentos])
 
-    def obtener_pasiente(self):
-        return json.dumps([ob.__dict__ for ob in self.pasientes])
-        
+    def obtener_paciente(self):
+        return json.dumps([ob.__dict__ for ob in self.pacientes])
+
+    def obtener_doctor(self):
+        return json.dumps([ob.__dict__ for ob in self.doctores])  
+
+    def obtener_enfermera(self):
+        return json.dumps([ob.__dict__ for ob in self.enfermeras])   
+
     #Update
-    def actualizar_datos(self, nombres, precio, nuevoprecio,descripcion, nuevadescripcion,  cantidad, nuevacantidad ):
+    def actualizar_medicamentos(self, nombres, precio, nuevoprecio,descripcion, nuevadescripcion,  cantidad, nuevacantidad ):
         for x in self.medicamentos:
             if x.nombre==nombres:
                 self.medicamentos[self.medicamentos.index(x)]=Medicamento(nombres, nuevoprecio, nuevadescripcion, nuevacantidad)
+                return True
+        return False
+
+    def actualizar_doctores(self, nombres, apellido, nacimiento, nuevonacimiento, genero, nuevogenero, usuario, nuevousuario, contraseña, nuevacontraseña, especialidad, nuevaespecialidad, telefono, nuevotelefono ):
+        for x in self.doctores:
+            if x.nombre==nombres and x.apellido==apellido  :
+                self.doctores[self.doctores.index(x)]=Doctor(nombres, apellido, nuevonacimiento, nuevogenero, nuevousuario, nuevacontraseña, nuevaespecialidad, nuevotelefono)
+                return True
+        return False
+
+    def actualizar_enfermeras(self, nombres, apellido, nacimiento, nuevonacimiento, genero, nuevogenero, usuario, nuevousuario, contraseña, nuevacontraseña,  telefono, nuevotelefono ):
+        for x in self.enfermeras:
+            if x.nombre==nombres and x.apellido==apellido  :
+                self.enfermeras[self.enfermeras.index(x)]=Enfermera(nombres, apellido, nuevonacimiento, nuevogenero, nuevousuario, nuevacontraseña,  nuevotelefono)
+                return True
+        return False    
+
+    def actualizar_pacientes(self, nombres, apellido, nacimiento, nuevonacimiento, genero, nuevogenero, usuario, nuevousuario, contraseña, nuevacontraseña,  telefono, nuevotelefono ):
+        for x in self.pacientes:
+            if x.nombre==nombres and x.apellido==apellido  :
+                self.pacientes[self.pacientes.index(x)]=Paciente(nombres, apellido, nuevonacimiento, nuevogenero, nuevousuario, nuevacontraseña,  nuevotelefono)
                 return True
         return False
 
@@ -40,4 +81,42 @@ class Gestor:
                 return True
         return False        
 
+    def eliminar_doctor(self, nombres, apellido):
+        for x in self.doctores:
+            if x.nombre==nombres and x.apellido==apellido:
+                self.doctores.remove(x)
+                return True
+        return False 
             
+    def eliminar_enfermera(self, nombres, apellido):
+        for x in self.enfermeras:
+            if x.nombre==nombres and x.apellido==apellido:
+                self.enfermeras.remove(x)
+                return True
+        return False
+
+    def eliminar_paciente(self, nombres, apellido):
+        for x in self.pacientes:
+            if x.nombre==nombres and x.apellido==apellido:
+                self.pacientes.remove(x)
+                return True
+        return False              
+
+    #Iniciar sesion 
+    def iniciar_sesion(self,user,password):
+        for x in self.pacientes:
+            if x.contraseña==password and x.usuario ==user:
+                return json.dumps(x.__dict__)
+        return '{"nombre":"false"}'
+
+    def registrar_paciente(self,nombre,apellido,nacimiento, genero, usuario, contraseña, telefono):
+        self.pacientes.append(Paciente(nombre,apellido,nacimiento, genero, usuario, contraseña, telefono))
+
+    def cargamasiva(self,data):
+        hola = re.split('\n',data)
+        print(hola[1])
+        i=1
+        while i < len(hola):
+            texto = re.split(',',hola[i])
+            self.crearPaciente(texto[0],texto[1],texto[2],texto[3],texto[4],texto[5],texto[6])
+            i = i+1 
--- BackEnd/index.py
+++ BackEnd/index.py
@@ -12,21 +12,96 @@ gestor= Gestor()
 # EndPoints
 @app.route('/',methods=['GET'])
 def home():
-    return 'SERVER IS WORKING !!!'
+    return 'HUGO SI LE SABE AL SHIT POST !!!'
 
 @app.route('/obtenermedicamentos')
 def obtenermedicamentos():
     return gestor.obtener_medicamento()
 
-@app.route('/obtenerpasientes')
-def obtenerpasientes():
-    return gestor.obtener_pasiente()
+@app.route('/obtenerpacientes')
+def obtenerpacientes():
+    return gestor.obtener_paciente()
 
-@app.route('/pasiente', methods=['POST'])
-def crearpasiente():
+@app.route('/obtenerdoctores')
+def obtenerdoctores():
+    return gestor.obtener_doctor()
+
+@app.route('/obtenerenfermeras')
+def obtenerenfermeras():
+    return gestor.obtener_enfermera()
+
+@app.route('/medicamentos', methods=['POST'])
+def crearmedicamento():
     dato = request.json
     gestor.crearMedicamento(dato['nombre'],dato['precio'],dato['descripcion'],dato['cantidad'])
     return '{"Estado":"Medicamento Creado"}'
+
+@app.route('/pacientes', methods=['POST'])
+def crearpaciente():
+    dato = request.json
+    gestor.crearPaciente(dato['nombre'],dato['apellido'],dato['nacimiento'],dato['genero'],dato['usuario'], dato['contraseña'],dato['telefono'])
+    return '{"Estado":"Paciente Creado"}'
+
+@app.route('/medicamentos/<nombre>', methods=['DELETE'])
+def eliminar_medicamento(nombre):
+    if(gestor.eliminar_medicamento(nombre)):
+        return '{"data":"Medicamento eliminado"}'
+    return '{"data":"Error"}'
+
+@app.route('/pacientes/<nombre>/<apellido>', methods=['DELETE'])
+def eliminar_paciente(nombre, apellido):
+    if(gestor.eliminar_paciente(nombre, apellido)):
+        return '{"data":"Paciente eliminado"}'
+    return '{"data":"Error"}'
+
+@app.route('/doctores/<nombre>/<apellido>', methods=['DELETE'])
+def eliminar_doctor(nombre, apellido):
+    if(gestor.eliminar_doctor(nombre, apellido)):
+        return '{"data":"Doctor eliminado"}'
+    return '{"data":"Error"}'
+
+@app.route('/enfermeras/<nombre>/<apellido>', methods=['DELETE'])
+def eliminar_enfermera(nombre, apellido):
+    if(gestor.eliminar_enfermera(nombre, apellido)):
+        return '{"data":"Enfermera eliminada"}'
+    return '{"data":"Error"}'
+
+@app.route('/medicamentos/<nombre>',methods=['PUT'])
+def actualizarmedicamento(nombre):
+    dato = request.json
+    if gestor.actualizar_medicamentos(nombre,dato['nombre'],dato['precio'],dato['descripcion'],dato['cantidad']):
+        return '{"data":"Actualizado"}'
+    return '{"data":"Error"}'
+
+@app.route('/pacientes/<nombre>',methods=['PUT'])
+def actualizarpaciente(nombre):
+    dato = request.json    
+
+    if gestor.actualizar_pacientes(nombre,dato['nombre'],dato['apellido'],dato['nacimiento'],dato['genero'],dato['usuario'], dato['contraseña'],dato['telefono']):
+        return '{"data":"Actualizado"}'
+    return '{"data":"Error"}'
+       
+@app.route('/login/<user>/<password>')
+def login(user,password):
+    print('entra')
+    return gestor.iniciar_sesion(user,password)
+
+@app.route('/registro',methods=['POST'])
+def registrar():
+    dato = request.json
+    gestor.registrar_paciente(dato['nombre'],dato['apellido'],dato['nacimiento'],dato['genero'],dato['usuario'], dato['contraseña'],dato['telefono'])    
+    return '{"data":"Creado"}'
+
+@app.route('/carga',methods=['POST'])
+def carga():
+    dato = request.json
+    gestor.cargamasiva(dato['data'])
+    return '{"data":"Cargados"}'
+
+
+
+
+
 
 #INICIAR EL SERVIDOR 
 
